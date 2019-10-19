9235afcb8a0052895bdae807201da3b7e2afcac7
bradleypaul <bradleypauld@gmail.com>
2019-10-19 16:58:39 -0500

    update broken project

--- frontend/README.md
+++ frontend/README.md
@@ -1,6 +1,6 @@
-# recipes
+# Cookbook
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 7.3.9.
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 8.3.10.
 
 ## Development server
 
--- frontend/e2e/src/app.e2e-spec.ts
+++ frontend/e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to recipes!');
+    expect(page.getTitleText()).toEqual('cookbook app is running!');
   });
 
   afterEach(async () => {
